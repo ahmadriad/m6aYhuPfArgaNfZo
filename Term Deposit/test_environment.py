f7eaabd512e13d3ddf61ba817e8eef56be251129
Ahmad Riad Al-adaileh <128195215+aladaileh@users.noreply.github.com>
2023-05-26 03:49:39 +0400

    Update test_environment.py

--- Term Deposit/test_environment.py
+++ Term Deposit/test_environment.py
@@ -1,25 +1,34 @@
-import sys
-
-REQUIRED_PYTHON = "python3"
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sb
+from sklearn.model_selection import GridSearchCV
+import category_encoders as ce
+from sklearn.preprocessing import LabelEncoder
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, mean_squared_error,f1_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold
 
 
 def main():
-    system_major = sys.version_info.major
-    if REQUIRED_PYTHON == "python":
-        required_major = 2
-    elif REQUIRED_PYTHON == "python3":
-        required_major = 3
-    else:
-        raise ValueError("Unrecognized python interpreter: {}".format(
-            REQUIRED_PYTHON))
+    data = pd.read_csv("C:/Users/97155/Downloads/term-deposit-marketing-2020.csv")
 
-    if system_major != required_major:
-        raise TypeError(
-            "This project requires Python {}. Found: Python {}".format(
-                required_major, sys.version))
-    else:
-        print(">>> Development environment passes all tests!")
+    data = split_data(data)
 
+  
+    xgb_model = train_model(undersampled_features, undersampled_target)
 
-if __name__ == '__main__':
+    metrics = get_model_metrics(model)
+
+    model_name = "XGBoost_model.pkl"
+
+    joblib.dump(value=xgb_model, filename=model_name)
+
+if __name__ == "__main__":
     main()
