f0fa5ddae8dbf3b4276bc480e69e465875795825
Ahmad Riad <aadylih@gmail.com>
2023-05-26 02:09:18 +0400

    add al the files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict if the customer will subscribe (yes/no) to a term deposit (variable y)',
+    author='Ahmad',
+    license='',
+)
